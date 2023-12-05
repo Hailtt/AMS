7fdc0c3ac71303436949afc0f1ee78ceb80af666
None <thengo2108@gmail.com>
2023-12-05 16:49:39 +0700

    giao dien noi dung chung tu

---
+++ fe/src/Page/DetailChungTu/DetailChungTu.jsx
@@ -0,0 +1,15 @@
+import NoiDungChungTu from "../../Component/ChiTietChungTu/NoiDungChungTu"
+
+function DetailChungTu() {
+    return (
+        <div className="DTCT">
+            <NoiDungChungTu />
+
+            <div className="track-log">
+                
+            </div>
+        </div>
+    )
+}
+
+export default DetailChungTu
