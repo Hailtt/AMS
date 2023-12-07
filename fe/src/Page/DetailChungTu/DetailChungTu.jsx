e3d5a0d5db9db05553bb16cba3693ba6e72dfa09
None <thengo2108@gmail.com>
2023-12-07 16:57:49 +0700

    tiep phan chi tiet chung tu

--- fe/src/Page/DetailChungTu/DetailChungTu.jsx
+++ fe/src/Page/DetailChungTu/DetailChungTu.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import NoiDungChungTu from "../../Component/ChiTietChungTu/NoiDungChungTu";
-import { DATA_NghiPhep } from "./data";
+import TrackLog from "../../Component/ChiTietChungTu/TrackLog";
+import Loading from "../Loading/Loading";
 function DetailChungTu() {
-	const { tenchungtu, form } = DATA_NghiPhep;
+	const [detail, setDetail] = useState(null);
+	const [diary, getDiary] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+
+	useEffect(()=>{
+		setLoading(true);
+		axios.get('http://192.168.137.160:8080/api/chungtu/noi-dung/CT12230001')
+		.then( res => {
+			setDetail(res.data);
+			
+
+		})
+		.catch( err => {
+			alert(err);
+		})
+
+		axios.get("http://192.168.137.160:8080/api/chungtu/nhat-ki/CT12230001")
+		.then(res => {
+			getDiary(res.data);
+		})
+		.catch(err => {
+			alert(err);
+		})
+		setLoading(false);
+	}, [])
+
+
+
 	return (
 		<div className="DTCT">
-			<NoiDungChungTu data={form.data} tenchungtu={tenchungtu} />
-			<div className="track-log"></div>
+			{loading && (
+				<div className="loading-screen">
+					<Loading />
+				</div>
+			)}
+			{detail  && <NoiDungChungTu detail={detail}/>}
+			
+			<div className="right">
+				{diary && <TrackLog diary={diary}/>}
+			</div>
 		</div>
 	);
 }
