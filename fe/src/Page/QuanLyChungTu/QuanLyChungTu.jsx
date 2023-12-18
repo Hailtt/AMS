8c2da36d034be0c2102de0ffa71e379bfa1541ce
dabeii <tranlan0310@gmail.com>
2023-12-19 02:43:09 +0700

    update

--- fe/src/Page/QuanLyChungTu/QuanLyChungTu.jsx
+++ fe/src/Page/QuanLyChungTu/QuanLyChungTu.jsx
@@ -54,10 +54,11 @@ function QuanLyChungTu({ loading, setLoading }) {
 	};
 
 	const getLoaiChungTu = async () => {
+		const token = localStorage.getItem("myToken");
 		setLoading(true);
 		let data = await new Promise((resolve, reject) => {
 			axios
-				.get(`${process.env.REACT_APP_BE_URL}/chung-tu/get-loai-chung-tu/1`)
+				.get(`${process.env.REACT_APP_BE_URL}/chung-tu/get-loai-chung-tu/1/${token}`)
 				.then((data) => {
 					resolve(data);
 					setType(data.data);
